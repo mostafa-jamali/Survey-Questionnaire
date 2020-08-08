2e981fa5805f6c828fba91e0c1976d4e6c6652e5
mostafa jamali <m.jamali20274@gmail.com>
2020-08-09 02:06:30 +0430

    Updates

---
+++ precache-manifest.cfbef2ba3ec8af1e9638eb57886317f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0da243bb25accf85cd02d68071480d",
+    "url": "/Survey-Questionnaire/index.html"
+  },
+  {
+    "revision": "c5f9349645ae3b1f06fa",
+    "url": "/Survey-Questionnaire/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "719491dd78b45ca9b789",
+    "url": "/Survey-Questionnaire/static/css/main.feaf5fbc.chunk.css"
+  },
+  {
+    "revision": "c5f9349645ae3b1f06fa",
+    "url": "/Survey-Questionnaire/static/js/2.f4f30119.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Survey-Questionnaire/static/js/2.f4f30119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719491dd78b45ca9b789",
+    "url": "/Survey-Questionnaire/static/js/main.1abbc0cf.chunk.js"
+  },
+  {
+    "revision": "1413bff3314b2ea58196",
+    "url": "/Survey-Questionnaire/static/js/runtime-main.b61095a1.js"
+  },
+  {
+    "revision": "feff963fc8b3ade0dc1d8413a44a476b",
+    "url": "/Survey-Questionnaire/static/media/SignPainterHouseScriptRegular.feff963f.ttf"
+  },
+  {
+    "revision": "7296ae79f6faca68798b00fbbbee4c22",
+    "url": "/Survey-Questionnaire/static/media/YekanBakh-Regular.7296ae79.ttf"
+  }
+]);
